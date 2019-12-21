b6ce53c24c4252fd1d97addbfe0acd308507d8f1
neal plotnik <nealplotnik@gmail.com>
2019-12-20 22:03:22 -0600

    added nltk

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from flask import Flask
 import os
+import nltk
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return "Hello World! Python"
+        return "Hello World! Python! NLTK!"
 
 if __name__ == "__main__":
   port = int(os.getenv("PORT", 8080))
